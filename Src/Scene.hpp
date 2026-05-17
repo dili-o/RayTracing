a29ff8af83c966d043ba430caf20ad6b987a149a
Dilichukwu Okoli <diliokoli@gmail.com>
2026-05-17 11:14:09 +0100

    Removed unsused files

--- Src/Scene.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-// Vendor
-#include <filesystem>
-
-class Renderer;
-
-void load_default_scene(Renderer* renderer);
-bool load_scene(const std::filesystem::path &scene_name, Renderer* renderer);
